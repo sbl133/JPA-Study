94183d0fb608cb724a7f0bf6cb8d6944934cfd9f
Bum <sbl133@naver.com>
2021-12-24 11:48:50 +0900

    feat: basic mapping

---
+++ jpa_basic/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        // code
+
+        try{
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+
+}
