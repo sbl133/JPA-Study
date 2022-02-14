a50edd588d686c8a208ddf1b330497fe50bcbcf2
Bum <sbl133@naver.com>
2022-02-14 15:15:09 +0900

    feat: teat

--- jap-basic/src/main/java/hellojpa/JpaMain.java
+++ jap-basic/src/main/java/hellojpa/JpaMain.java
@@ -57,6 +57,12 @@ public class JpaMain {
             em.flush();
             em.clear();
 
+            String query = "select m from Member m";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println(member.getUsername());
+            }
 //            String query = "select distinct t from Team t join fetch t.members";
 //
 //            List<Team> result = em.createQuery(query, Team.class).getResultList();
--- jap-basic/src/main/java/hellojpa/Member.java
+++ jap-basic/src/main/java/hellojpa/Member.java
@@ -24,7 +24,7 @@ public class Member {
     private String username;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID", updatable = false, insertable = false)
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     // 주소
