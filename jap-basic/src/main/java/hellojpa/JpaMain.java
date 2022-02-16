93157d7e11efd74a3431555da08d4717615b98ab
Bum <sbl133@naver.com>
2022-02-16 21:54:06 +0900

    feat: study about persistContext

--- jap-basic/src/main/java/hellojpa/JpaMain.java
+++ jap-basic/src/main/java/hellojpa/JpaMain.java
@@ -23,57 +23,66 @@ public class JpaMain {
         // code
 
         try {
+            Team team1 = new Team();
+            team1.setName("팀1");
+            em.persist(team1);
 
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team1);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team1);
             em.persist(member2);
-
-            Team team = new Team();
-            team.setName("팀1");
-            em.persist(team);
-
-            team.getMembers().add(member1);
-            team.getMembers().add(member2);
-
-            Member member3 = new Member();
-            member3.setUsername("관리자3");
-            em.persist(member3);
-
-            Member member4 = new Member();
-            member4.setUsername("관리자4");
-            em.persist(member4);
 
             Team team2 = new Team();
             team2.setName("팀2");
             em.persist(team2);
 
-            team2.getMembers().add(member3);
-            team2.getMembers().add(member4);
+            Member member3 = new Member();
+            member3.setUsername("관리자3");
+            member3.setTeam(team2);
+            em.persist(member3);
+
+            Member member4 = new Member();
+            member4.setUsername("관리자4");
+            member4.setTeam(team2);
+            em.persist(member4);
 
             em.flush();
             em.clear();
 
-            String query = "select m from Member m";
-            List<Member> result = em.createQuery(query, Member.class)
+            List<Member> members = em.createQuery("select m from Member m", Member.class)
                     .getResultList();
-            for (Member member : result) {
-                System.out.println(member.getUsername());
+            List<Team> teams = em.createQuery("select t from Team t", Team.class)
+                    .getResultList();
+
+            for (Member member : members) {
+                System.out.println("member = " + member);
+                System.out.println(member.getTeam());
+                System.out.println(member.getTeam().getName());
             }
+
+//            for (Team team : teams) {
+//                System.out.println("team = " + team);
+//                System.out.println(team.getMembers());
+//                System.out.println(team.getMembers().get(0).getUsername());
+//            }
+
+
 //            String query = "select distinct t from Team t join fetch t.members";
 //
 //            List<Team> result = em.createQuery(query, Team.class).getResultList();
-//            for (Team testTeam : result) {
-//                for (Member member : testTeam.getMembers()) {
+//            for (Team team : result) {
+//                for (Member member : team.getMembers()) {
 //                    System.out.println(member.getUsername());
 //                }
 //            }
 //            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-//            for (Team testTeam : result) {
-//                for (Member member : testTeam.getMembers()) {
+//            for (Team team : result) {
+//                for (Member member : team.getMembers()) {
 //                    System.out.println(member.getTeam().getName());
 //                }
 //            }
