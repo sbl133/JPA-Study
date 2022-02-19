fdbd9177cf0b198e70b80d4e76f969afb0f4d3df
Bum <sbl133@naver.com>
2022-02-19 23:28:40 +0900

    feat: dynamicQueryUsingBuilder

---
+++ querydsl/src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,13 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+    // 회원명, 팀명, 나이(ageGoe, ageLod)
+
+    private String userName;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+}
